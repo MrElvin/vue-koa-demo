6ad0dcbb2c467112f16ec69aecb112e0bd4d3e99
BreezyMelon <935448078@qq.com>
2018-06-08 14:47:50 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:MrElvin/vue-koa-demo.git',
       path : '/home/breezymelon/vue-koa-demo',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && cd ./server && npm install && cd .. && npm run build  && ./run.sh && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
       user : 'breezymelon',
