1f151375c82662f503733552775cf874a9d1c1c1
BreezyMelon <935448078@qq.com>
2018-06-08 14:17:13 +0800

    add ecosystem

---
+++ server/ecosystem.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'vue-koa-todos',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'breezymelon',
+      host : '47.95.111.133',
+      ref  : 'origin/master',
+      repo : 'git@github.com:MrElvin/vue-koa-demo.git',
+      path : '/www/projects/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'breezymelon',
+      host : '47.95.111.133',
+      ref  : 'origin/master',
+      repo : 'git@github.com:MrElvin/vue-koa-demo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
